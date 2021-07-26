7a6375e59b07d062904ea3bd61da34e9384a1fde
wtae1216 <wtae1216@gmail.com>
2021-07-26 19:33:52 +0900

    테스트를 위한 Jest 설정 # rollup-builder

--- packages/rollup-builder/jest.config.js
+++ packages/rollup-builder/jest.config.js
@@ -1,5 +1,10 @@
 const jestBaseConfig = require('@choseohwan/jest-config-base');
 
 module.exports = {
-    ...jestBaseConfig
+    ...jestBaseConfig,
+    testMatch: ["<rootDir>/**/*.spec.[jt]s?(x)"],
+    moduleNameMapper: {
+        '~src/(.*)': '<rootDir>/src/$1',
+        '~types/(.*)': '<rootDir>/types/$1'
+    }
 };
