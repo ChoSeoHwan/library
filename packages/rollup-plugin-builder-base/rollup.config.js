6a1ec7c525606bf23f738e3bff5525dda3cfa493
ChoSeoHwan <wtae1216@naver.com>
2021-08-21 21:04:50 +0900

    feat(rollup-plugin-builder-base): 기존 rollup-builder 에 있던 plugins builder 분리
    pluginsBuilder 가 많아질수록 peerDependency 가 많아지는 구조 => 라이브러리 분리하여 필요한 builder 만 쓰도록변경

---
+++ packages/rollup-plugin-builder-base/rollup.config.js
@@ -0,0 +1,86 @@
+import buildRollupOptions, {Input, OutputBuilder, Plugin, Plugins} from '@choseohwan/rollup-builder';
+import cleaner from "rollup-plugin-cleaner";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript, {RPT2Options} from "rollup-plugin-typescript2";
+import babel from "@rollup/plugin-babel";
+import {DEFAULT_EXTENSIONS} from "@babel/core";
+import {terser} from "rollup-plugin-terser";
+import ttypescript from 'ttypescript';
+
+const input = new Input('src/buildBasePlugins.ts');
+
+const outputBuilder = new OutputBuilder({
+    sourcemap: true
+});
+
+const plugins = new Plugins(
+    new Plugin(
+        'rollup-builder-cleaner',
+        cleaner,
+        (output) => [
+            {
+                targets: [output.getOutputDir()]
+            }
+        ]
+    ),
+    new Plugin(
+        'rollup-plugin-peer-deps-external',
+        peerDepsExternal,
+        []
+    ),
+    new Plugin('@rollup/plugin-node-resolve', resolve, [
+        {
+            browser: true
+        }
+    ]),
+    new Plugin(
+        'rollup-plugin-typescript2',
+        typescript,
+        (output) => [
+            {
+                typescript: ttypescript,
+                tsconfigDefaults: {
+                    compilerOptions: {
+                        outDir: output.getOutputDir(),
+                        plugins: [
+                            {
+                                transform: 'typescript-transform-paths',
+                                afterDeclarations: true
+                            }
+                        ]
+                    }
+                }
+            }
+        ]
+    ),
+    new Plugin('@rollup/plugin-commonjs', commonjs, [
+        {
+            extensions: ['.js', '.ts'],
+            include: ['node_modules/**']
+        }
+    ]),
+    new Plugin('@rollup/plugin-babel', babel, [
+        {
+            extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
+            babelHelpers: 'runtime',
+            plugins: [
+                '@babel/plugin-transform-runtime',
+                '@babel/plugin-transform-arrow-functions'
+            ]
+        }
+    ]),
+    new Plugin('rollup-plugin-terser', terser, [])
+);
+
+export default buildRollupOptions(
+    input,
+    [
+        outputBuilder.buildCJS({
+            exports: 'named',
+            dir: 'dist'
+        })
+    ],
+    plugins
+);
