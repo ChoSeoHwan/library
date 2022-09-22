e4666f1e0706eed851b2f1a9ed38eb0f54e91b6b
아이작_조서환 <wtae1216@afreecatv.com>
2022-09-22 14:28:01 +0900

    feat: 각종 라이브러리 yarn berry 방식으로 업그레이드

--- packages/react-utils/.jest/jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    preset: "@choseohwan/jest-preset-base",
-    rootDir: '../',
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/.jest/jest.setup.ts'],
-    moduleNameMapper: {
-        '~src/(.*)': '<rootDir>/src/$1',
-    }
-};
